40f004b3a5c552b082a44e87dfd969acecee0dc2
Colby Fayock <fayock@gmail.com>
2024-06-11 08:31:01 -0400

    Step 0: Adding a new API endpoint that returns NASA's image of the day

---
+++ api/image.ts
@@ -0,0 +1,4 @@
+export async function GET(request: Request) {
+  const results = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(r => r.json());
+  return new Response(JSON.stringify(results));
+}
