e576ab15a00d8887496ca76abbb9b8ac359fdb19
Colby Fayock <fayock@gmail.com>
2024-06-12 22:19:12 -0400

    Step 3: Dynamically setting response headers allowed origin

--- api/image.ts
+++ api/image.ts
@@ -1,8 +1,16 @@
+const ALLOWED_ORIGINS = [
+  `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
+  'https://colbyfayock.com',
+  'https://spacejelly.dev'
+];
+
 export async function GET(request: Request) {
+  const requestOrigin = request.headers.get('origin');
+  const accessOrigin = ALLOWED_ORIGINS.find(origin => origin === requestOrigin) || ALLOWED_ORIGINS[0];
   const results = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(r => r.json());
   return new Response(JSON.stringify(results), {
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': accessOrigin,
     }
   });
 }
