af3f1eeda6a027bc4bd2478ce88850af10d4c382
Colby Fayock <fayock@gmail.com>
2024-06-12 22:38:24 -0400

    Step 4: Configuring OPTIONS request for preflight request

--- api/image.ts
+++ api/image.ts
@@ -4,13 +4,26 @@ const ALLOWED_ORIGINS = [
   'https://spacejelly.dev'
 ];
 
-export async function GET(request: Request) {
+export function OPTIONS(request: Request) {
+  const requestOrigin = request.headers.get('origin');
+  const accessOrigin = ALLOWED_ORIGINS.find(origin => origin === requestOrigin) || ALLOWED_ORIGINS[0];
+  return new Response(JSON.stringify({ status: 'Ok' }), {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': accessOrigin,
+      'Access-Control-Allow-Headers': 'Content-Type',
+    }
+  });
+}
+
+export async function POST(request: Request) {
   const requestOrigin = request.headers.get('origin');
   const accessOrigin = ALLOWED_ORIGINS.find(origin => origin === requestOrigin) || ALLOWED_ORIGINS[0];
   const results = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(r => r.json());
   return new Response(JSON.stringify(results), {
     headers: {
       'Access-Control-Allow-Origin': accessOrigin,
+      'Access-Control-Allow-Headers': 'Content-Type',
     }
   });
 }
