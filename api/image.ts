c03fd9783292feca2e6431eac420e804ce9ed2f3
Colby Fayock <fayock@gmail.com>
2024-06-12 21:51:29 -0400

    Step 1: Enabling CORS for a single origin

--- api/image.ts
+++ api/image.ts
@@ -1,4 +1,8 @@
 export async function GET(request: Request) {
   const results = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(r => r.json());
-  return new Response(JSON.stringify(results));
+  return new Response(JSON.stringify(results), {
+    headers: {
+      'Access-Control-Allow-Origin': 'https://spacejelly.dev',
+    }
+  });
 }
